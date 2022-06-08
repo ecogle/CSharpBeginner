f67c97cba24769b3a6e9835c56319b073709ece3
ecogl <ecogl@DESKTOP-FUNQJJP>
2022-06-08 11:09:12 -0500

    Add project files.

---
+++ CSharpBeginner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
