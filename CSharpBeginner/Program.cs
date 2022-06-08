1f5efcbd8af7231d8a4b9e0af8b0cc47a5de204f
ecogle <ecogle@gmail.com>
2022-06-08 11:36:57 -0500

    First commit

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -6,7 +6,17 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Interpolated String
+            var name = "Chad";
+            Console.WriteLine($"This is a test {name}");
+
+            //WriteLine calls string.Format
+            Console.WriteLine("This is another test for {0}", name);
+
+            //Using the string format
+            Console.WriteLine(string.Format("This is a test with string.Format for {0}",name));
+
+
         }
     }
 }
